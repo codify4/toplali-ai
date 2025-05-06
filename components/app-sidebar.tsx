b49bcda81e375fae1942288733be592abab538a2
codify4 <kushta.joni@gmail.com>
2025-05-06 20:35:44 +0200

    fix: sidebar changes

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -2,23 +2,16 @@
 
 import * as React from "react"
 import {
-  BookOpen,
-  Bot,
-  Command,
+  File,
   FilePlus2,
-  Frame,
   GalleryVerticalEnd,
+  Home,
+  Info,
   LifeBuoy,
-  Map,
-  PieChart,
-  Send,
-  Settings2,
-  SquareTerminal,
+  Settings2
 } from "lucide-react"
 
 import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavSecondary } from "@/components/nav-secondary"
 import { NavUser } from "@/components/nav-user"
 import {
   Sidebar,
@@ -26,9 +19,10 @@ import {
   SidebarFooter,
   SidebarHeader,
   SidebarMenu,
-  SidebarMenuButton,
   SidebarMenuItem,
+  SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { Button } from "./ui/button"
 
 const data = {
   user: {
@@ -37,7 +31,31 @@ const data = {
     avatar: "/avatars/shadcn.jpg",
   },
   navMain: [
-    
+    {
+      title: "Home",
+      url: "/",
+      icon: Home,
+    },
+    {
+      title: "Past Conversations",
+      url: "/past-conversations",
+      icon: File,
+    },
+    {
+      title: "Settings",
+      url: "/settings",
+      icon: Settings2,
+    },
+    {
+      title: "Help",
+      url: "/help",
+      icon: LifeBuoy,
+    },
+    {
+      title: "About",
+      url: "/about",
+      icon: Info,
+    },
   ],
   navSecondary: [
     
@@ -53,9 +71,14 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarHeader className="bg-primary">
         <SidebarMenu>
           <SidebarMenuItem className="flex flex-row items-center justify-between gap-2">
-            <GalleryVerticalEnd size={20} className="cursor-pointer hover:text-muted" />
             <h3 className="font-bold text-lg">Top Lali AI</h3>
-            <FilePlus2 size={20} className="cursor-pointer hover:text-muted" />
+            <Button
+              variant="ghost"
+              size="icon"
+              className="size-10"
+            >
+              <FilePlus2 size={24} />
+            </Button>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
@@ -63,6 +86,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavMain items={data.navMain} />
       </SidebarContent>
       <SidebarFooter className="bg-primary">
+        {/* <Button variant="ghost" className="w-full hover:bg-muted/10 flex items-center justify-start text-start">
+          <Trash2 size={16} />
+          Clear Chat
+        </Button> */}
         <NavUser user={data.user} />
       </SidebarFooter>
     </Sidebar>
--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ChevronRight, type LucideIcon } from "lucide-react"
+import { ChevronRight, Trash2, type LucideIcon } from "lucide-react"
 
 import {
   Collapsible,
@@ -40,10 +40,11 @@ export function NavMain({
         {items.map((item) => (
           <Collapsible key={item.title} asChild defaultOpen={item.isActive}>
             <SidebarMenuItem>
-              <SidebarMenuButton asChild tooltip={item.title}>
+              <SidebarMenuButton asChild tooltip={item.title} className="group hover:bg-muted/10 hover:text-black dark:hover:text-white py-5">
                 <a href={item.url}>
-                  <item.icon />
+                  <item.icon size={18} />
                   <span>{item.title}</span>
+                  <Trash2 size={16} className="ml-auto hover:text-red-700"/>
                 </a>
               </SidebarMenuButton>
               {item.items?.length ? (
