2e106e2144c2e2dc195ff6fb08762660bc4ccde0
codify4 <kushta.joni@gmail.com>
2025-05-21 17:53:38 +0200

    added: insta send to chat if user

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,6 +1,13 @@
+import { getSession } from '@/actions/auth-actions';
 import SignInForm from '@/app/signin/components/login-form'
+import { redirect } from 'next/navigation';
 
-function SignInPage() {
+async function SignInPage() {
+    const session = await getSession();
+    if (session) {
+        redirect("/chat");
+    }
+    
     return (
         <div className="flex min-h-svh flex-col items-center justify-center p-6">
             <SignInForm />
